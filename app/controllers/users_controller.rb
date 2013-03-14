37cc4878933635094977e3b44bfa4ae583c4d699
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-14 14:31:43 +0100

    generate users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+
+  # GET /users.json
+  def index
+    users = User.all
+    render json: users, meta: { total: users.count }
+  end
+
+  # GET /users/1.json
+  def show
+    requires_parameters(*[:id])
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  # POST /users.json
+  def create
+    requires_parameters(*minimum_parameter_keys)
+    user = User.create(min_params)
+    if user.save
+      render json: user, location: user_path(user), status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH|PUT /users/1.json
+  def update
+    requires_parameters(*minimum_parameter_keys)
+    requires_parameters(*[:id])
+    user = User.find(params[:id])
+
+    if user.update_attributes(min_params)
+      head :no_content
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1.json
+  def destroy
+    requires_parameters(*[:id])
+    user = User.find(params[:id])
+    user.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def minimum_parameter_keys
+    [:email, :name]
+  end
+
+  def min_params
+    params.slice(*minimum_parameter_keys)
+  end
+end
