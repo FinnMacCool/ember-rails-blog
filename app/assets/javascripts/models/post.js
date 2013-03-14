37532567fef1fa4a50f138a1c3e4bce6b74b005a
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-14 11:42:08 +0100

    adapt ember.js post model to new attributes

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -1,11 +1,16 @@
 EmberBlog.Post = DS.Model.extend({
+    user: DS.belongsTo('EmberBlog.User'),
     comments: DS.hasMany('EmberBlog.Comment'),
 
-    tagList: DS.attr('raw'),
-    title: DS.attr('string'),
     body: DS.attr('string'),
+    tagList: DS.attr('raw'),
+    teaser: DS.attr('string'),
+    title: DS.attr('string'),
+    userId: DS.attr('number'),
+
     createdAt: DS.attr('date'),
     updatedAt: DS.attr('date'),
+
     htmlId: function() {
         return "post-" + this.get('clientId');
     }.property('clientId')
