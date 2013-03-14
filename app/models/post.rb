067e849718b488d5d6d6aa5fcba4e9fd490a7495
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-14 11:07:16 +0100

    adapt post model to new attributes and establish relationship with user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,13 @@
 class Post < ActiveRecord::Base
   acts_as_ordered_taggable
-  attr_accessible :body, :title#, :tag_list
+  attr_accessible :body, :teaser, :title#, :tag_list
   attr_protected nil
 
   has_many :comments
+  belongs_to :author, class_name: "User"
 
   validates_presence_of :body
+  validates_length_of :teaser, {maximum: 500}
+  validates_presence_of :teaser
   validates_presence_of :title
 end
