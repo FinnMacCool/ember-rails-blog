803851378ac608b43b360fbc34e773c64c967f83
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-18 17:44:23 +0100

    add alternative functionality to posts_new_route

--- app/assets/javascripts/routes/posts_new_route.js
+++ app/assets/javascripts/routes/posts_new_route.js
@@ -1,7 +1,14 @@
 EmberBlog.PostsNewRoute = Ember.Route.extend({
     model: function() {
+        //return EmberBlog.Post.createRecord();
         return null;
     },
+    /*
+    setupController: function( controller, model ) {
+        return this._super(),
+            controller.set( 'content', model );
+    }
+    */
 
     setupController: function(controller) {
         controller.startEditing();
@@ -11,4 +18,5 @@ EmberBlog.PostsNewRoute = Ember.Route.extend({
         this._super();
         this.controllerFor('posts.new').stopEditing();
     }
+
 });
