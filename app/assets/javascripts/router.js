abfa4e2215cb9e3aafc4859d475c72471126ec4a
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-18 15:35:13 +0100

    make the routes for post show pages and new posts nested routes

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,9 +1,12 @@
 EmberBlog.Router.map(function() {
     // default index route to / loading index template
     this.resource("posts", function() { // implicit path and template name (same as route name)
-        //this.route('new');
+        //this.route('index');
+        this.route('new');
+        this.resource('post', { path: '/:post_id' });
     });
-    this.resource('post', { path: '/posts/:post_id' });
+    //this.route('posts.new', { path: '/posts/new' });
+    //this.resource('post', { path: '/posts/:post_id' });
     this.resource("users", function() {
 
     });
