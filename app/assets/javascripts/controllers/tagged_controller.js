c4e05704b7b7ad3fed4bbb3d2ebd7290c19798a0
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-19 17:45:58 +0100

    make links to tags work (finally)

--- app/assets/javascripts/controllers/tagged_controller.js
+++ app/assets/javascripts/controllers/tagged_controller.js
@@ -1,3 +1,8 @@
-EmberBlog.TaggedController = Ember.ArrayController.extend({
-    content: []
+EmberBlog.TaggedController = Ember.ObjectController.extend({
+    content: null,
+
+    postsWithTag: function() {
+        var posts = EmberBlog.Post.find({'tag_name': this.get('content')});
+        return posts;
+    }.property('@content')
 });
--- app/assets/javascripts/routes/tagged_route.js
+++ app/assets/javascripts/routes/tagged_route.js
@@ -2,10 +2,7 @@ EmberBlog.TaggedRoute = Ember.Route.extend({
     model: function(params) {
         Em.Logger.info( params );
 
-        var posts = EmberBlog.Post.find({'tag_name': params.tag_name});
-        return posts;
-    }//,
-    //controllerFor: function() {
-    //    return EmberBlog.TaggedController;
-    //}
+        var tag = params.tag_name;
+        return tag;
+    }
 });
