1a75ca2b8894cd854095df196e9f0aca3607c1d3
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-22 12:59:04 +0100

    add function onSave to post_index_controller

--- app/assets/javascripts/controllers/post_index_controller.js
+++ app/assets/javascripts/controllers/post_index_controller.js
@@ -1,3 +1,10 @@
 EmberBlog.PostIndexController = Ember.ObjectController.extend({
-    content: null
+    content: null,
+
+    onSave: function(text) {
+        var transaction = this.get('store').transaction();
+        transaction.createRecord(EmberBlog.Comment, {text: text, userId: 1, postId: 1});
+        transaction.commit();
+        transaction = null;
+    }
 });
