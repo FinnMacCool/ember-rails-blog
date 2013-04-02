598fa60718dba7309093d1b23f1dc7e69abd92f4
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-04-02 11:56:18 +0200

    establish 1:n relationship between models category and post

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
 
+  has_many :posts
+
   validates_presence_of :name
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
   acts_as_ordered_taggable
-  attr_accessible :body, :teaser, :title#, :tag_list
+  attr_accessible :body, :category_id, :teaser, :title#, :tag_list
   attr_protected nil
 
+  belongs_to :category
   belongs_to :user
   has_many :comments
   has_many :likes, as: :likable
