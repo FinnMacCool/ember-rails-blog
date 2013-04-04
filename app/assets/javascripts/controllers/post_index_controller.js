f6feede7e171917acf7a8f7bfed12b8584d2a086
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-04-04 15:39:37 +0200

    add function destroy to post_index_controller

--- app/assets/javascripts/controllers/post_index_controller.js
+++ app/assets/javascripts/controllers/post_index_controller.js
@@ -1,3 +1,22 @@
 EmberBlog.PostIndexController = Ember.ObjectController.extend({
-    content: null
+  content: null,
+
+  destroy: function() {
+    this.transaction = this.get('store').transaction();
+    var content = this.get('content');
+    this.transaction.add(content);
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      content.deleteRecord();
+      Em.Logger.info(EmberBlog);
+      content.one('didDelete', function() {
+        Em.Logger.info(EmberBlog);
+        EmberBlog.Router.router.transitionTo('posts.index');
+      });
+      this.transaction.commit();
+    }
+    else{
+      this.transaction.rollback();
+      this.transaction = null;
+    }
+  }
 });
